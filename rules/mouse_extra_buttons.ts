27b88c5e3cc678e556e21e6ce9a43da449347391
Nilesh Parkhe <parkhenilesh@outlook.com>
2024-07-19 18:42:28 +0530

    Add Mission Control mapping to Mouse
    - Shift + Middle Button

--- rules/mouse_extra_buttons.ts
+++ rules/mouse_extra_buttons.ts
@@ -2,7 +2,7 @@ import { KarabinerRules } from "../types";
 
 export const mouseExtraButtons: KarabinerRules = {
   description:
-    "Maps Mouse button 5 to left desktop switch, 4 to right desktop switch",
+    "Maps Mouse button 5 to left desktop switch, 4 to right desktop switch, `Shift + Middle Button` to Mission Control",
   manipulators: [
     {
       from: {
@@ -23,6 +23,22 @@ export const mouseExtraButtons: KarabinerRules = {
       to: [
         {
           key_code: "right_arrow",
+          modifiers: ["left_control"],
+        },
+      ],
+      type: "basic",
+    },
+    {
+      from: {
+        pointing_button: "button3",
+        modifiers: {
+          mandatory: ["left_shift"],
+          optional: ["any"],
+        },
+      },
+      to: [
+        {
+          key_code: "up_arrow",
           modifiers: ["left_control"],
         },
       ],
